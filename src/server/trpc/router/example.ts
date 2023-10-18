a76b571447feeaf51d93dd16a4a0827587279ee1
shiro <shiro@usagi.io>
2023-10-19 08:57:53 +0900

    Update tanstack query, add API endpoint delay

--- src/server/trpc/router/example.ts
+++ src/server/trpc/router/example.ts
@@ -2,11 +2,13 @@ import { z } from "zod";
 import { procedure, router } from "../utils";
 
 export default router({
-  hello: procedure.input(z.object({ name: z.string() })).query(({ input }) => {
-    //console.log("API");
-    //await new Promise(r => {setTimeout(() => {r(0)},5000)});
+  hello: procedure.input(z.object({ name: z.string() })).query(async ({ input }) => {
+    console.log("API");
+    await new Promise(r => {setTimeout(() => {r(0)},5000)});
 
-    return `Hello ${input.name}`;
+    const time = +new Date();
+    console.log("DONE", time);
+    return `Hello ${input.name} (${time})`;
   }),
   random: procedure
     .input(z.object({ num: z.number() }))
